0c0408f20b25e1a7802d58c61159a3ce2ec9299d
NSC/Network Service Center <NSC@oocl.com>
2020-11-25 15:19:20 +0800

    [JavaOO][Dominic Leung] feat: Finish Practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,34 @@
 package practice06;
 
-public class Teacher {
+import practice05.Person;
+
+public class Teacher extends Person {
+    private Klass klass;
+    public Teacher(String name, int age,Klass klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name,age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(klass!=null)
+            return super.introduce()+String.format(" I am a Teacher. I teach Class %d.",this.klass.getNumber());
+        return super.introduce()+" I am a Teacher. I teach No Class.";
+    }
+
+
+    public String introduceWith(Student student) {
+        if (this.klass.getNumber()==student.getKlass().getNumber()){
+            return super.introduce()+String.format(" I am a Teacher. I teach %s.",student.getName());
+        }
+        return super.introduce()+String.format(" I am a Teacher. I don't teach %s.",student.getName());
+    }
 }
