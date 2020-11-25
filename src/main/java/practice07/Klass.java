6c0a7b296839736f31c3d213a366b3e316e5ad9b
NSC/Network Service Center <NSC@oocl.com>
2020-11-25 15:47:51 +0800

    [JavaOO][Dominic Leung] feat: Finish Practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,26 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d",this.number);
+    }
+
+    public void assignLeader(Student leader) {
+        this.leader=leader;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
 }
